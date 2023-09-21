a944bf6fc3013b8100bc374e260e4ede3854030b
mel148 <melodygatan@gmail.com>
2023-09-20 23:48:44 -0700

    update http?

--- client/src/pages/auth.jsx
+++ client/src/pages/auth.jsx
@@ -23,7 +23,7 @@ const Login = () => {
     const onSubmit = async (event) => {
         event.preventDefault();
         try {
-            const response = await axios.post("http://localhost:3001/auth/login", {
+            const response = await axios.post("https://mel-mern-recipe-app.vercel.app/auth/login", {
                 username,
                 password,
             });
@@ -54,7 +54,7 @@ const Register = () => {
     const onSubmit = async (event) => {
         event.preventDefault();
         try {
-            await axios.post("http://localhost:3001/auth/register", {
+            await axios.post("https://mel-mern-recipe-app.vercel.app/auth/register", {
                 username,
                 password,
             });
--- client/src/pages/saved-recipes.jsx
+++ client/src/pages/saved-recipes.jsx
@@ -10,7 +10,7 @@ export const SavedRecipes = () => {
         const fetchSavedRecipe = async () => {
             try {
                 const response = await axios.get(
-                    `http://localhost:3001/recipes/savedRecipes/${userID}`
+                    `https://mel-mern-recipe-app.vercel.app/recipes/savedRecipes/${userID}`
                 );
                 setSavedRecipes(response.data.savedRecipes);
             } catch (err) {
