1ead4221e99f1eb320de2d884a614f4826dee702
Melody <82139031+mel418@users.noreply.github.com>
2023-09-21 22:24:04 -0700

    root and port

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,7 @@ import 'dotenv/config';
 import {userRouter} from "./routes/users.js";
 import {recipesRouter} from "./routes/recipes.js";
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 app.use(cors());
@@ -18,7 +18,12 @@ const mongoPassword = process.env.MONGODB_PASSWORD;
 
 mongoose.connect(`mongodb+srv://melodygatan:${mongoPassword}@recipes.mznagvd.mongodb.net/recipes?retryWrites=true&w=majority`)
 
+const port = process.env.PORT || 3001;
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the server!"); 
+});
 
-app.listen(3001, () => console.log("SERVER STARTED!"));
+app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+
 
